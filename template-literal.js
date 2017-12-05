956dcd816388f13b04402d6cd19db93cc8d2a271
Sergio Soares <acad.sergio@gmail.com>
2017-12-05 15:34:47 -0300

    Template Literal codes

---
+++ template-literal.js
@@ -0,0 +1,15 @@
+
+const me = {
+    'name': 'Sergio',
+    'fruit': 'Grape'
+}
+const text = 'I am ' + me.name + ' and like ' + me.fruit + '!';
+
+const newText = `I am ${me.name} and like ${me.fruit}!`
+
+const fruits 
+= `grape,
+orange,
+caju`;
+
+console.log(fruits);
